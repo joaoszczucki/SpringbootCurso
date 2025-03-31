30c19e8c6c4e39169965777a7697881410f4f921
João <joao.pszczucki@gmail.com>
2025-03-31 06:22:08 -0300

    Commit Inicial

---
+++ src/main/java/br/com/curso/SpringbootCurso/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.curso.SpringbootCurso.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/test/java/br/com/curso/SpringbootCurso/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.SpringbootCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
